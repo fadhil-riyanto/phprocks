fe39867d146674c0e2aa5ed3b1e1db19fbcfbe7d
fadhil riyanto <me@fadev.org>
2024-07-27 21:29:46 +0700

    short test
    Signed-off-by: fadhil riyanto <me@fadev.org>

--- test/testconnect.php
+++ test/testconnect.php
@@ -2,6 +2,12 @@
 require_once("../src/Rocksclient.php");
 
 $rockserver = new FadhilRiyanto\Rocksclient("127.0.0.1", 8998);
-$rockserver->set("randomkey", "this is value sent by PHPaROCKS!");
 
-print_r($rockserver->get("randomkey"));
+// for($i = 0; $i < 100; $i++) {
+//         $rockserver->set("randomkeyof" . $i, "this is value sent by PHPaROCKS! indexof " . $i);
+        
+// }
+
+for($i = 0; $i < 100; $i++) {
+        echo $rockserver->get("randomkeyof" . $i);
+}
